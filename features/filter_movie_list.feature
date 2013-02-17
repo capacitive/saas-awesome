db3cae19599a5f4cef331847ebf9a054900a8799
eastfarthing <mark.cafazzo@gmail.com>
2013-02-16 20:57:26 -0800

    cuke all pass

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -19,24 +19,26 @@ Background: movies have been added to database
   | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
   | Chicken Run             | G      | 21-Jun-2000  |
 
-  And  I am on the RottenPotatoes home page
+  And I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
 
   Given all checkboxes are unchecked: PG, R, PG-13, G, NC-17
 
   # enter step(s) to check the 'PG' and 'R' checkboxes
-  When I check the following ratings: PG, R
-
-  # enter step to "submit" the search form on the homepage
+  Given I check the following ratings: PG, R
   When I press "ratings_submit"
-  Then show me the page
-
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  Then I should see the movies: "Amelie, The Terminator, When Harry Met Sally, The Incredibles, Raiders of the Lost Ark"
+  Then I should not see the movies: "Aladdin, The Help, Chocolat, 2001: A Space Odyssey, Chicken Run"
 
 Scenario: no ratings selected
-  # see assignment
+  #Given all checkboxes are unchecked: PG, R, PG-13, G, NC-17
+  Given I uncheck the following ratings: G, PG, PG-13, R
+  When I press "ratings_submit"
+  Then I should not see all of the movies
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: PG, R, PG-13, G
+  When I press "ratings_submit"
+  Then I should see all of the movies
+  Then show me the page
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /^the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create!(movie)
   end
-  assert movies_table.hashes .size == Movie.all.count
+  assert movies_table.hashes.size == Movie.all.count
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -15,18 +15,49 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
     assert titles.index(e1) < titles.index(e2)
 end
 
+Then /^I should totally see/ do
+
+end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-Given /^all checkboxes are unchecked: (.*)/ do |checkboxes|
+Given /^all checkboxes are (un)?checked: (.*)/ do |unchecked, checkboxes|
   checkboxes.split(/, /).each do |value|
-    uncheck("ratings_#{value}")
-    #"I uncheck" "ratings_#{value}"
+    if(unchecked.to_s.include?("un"))
+      uncheck("ratings_#{value}")
+    else
+      uncheck("ratings_#{value}")
+    end
   end
 end
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+Then /^I should( not)? see the movies: (.*)/ do |neg, checkboxes|
+  checkboxes.split(/, /).each do |value|
+    "Then I should#{neg} see '#{value}'"
+  end
+end
+
+Given /^all ratings are (un)?checked/ do |unchecked|
+  Movie.all_ratings.each do |rating|
+    if(unchecked.to_s.include?("un"))
+      uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
+end
+
+Then /^I should( not)? see all of the movies/ do |negpos|
+  if(negpos.to_s.include?("not"))
+    page.all("table#movies tbody tr").count.should == 0
+  else
+    page.all("table#movies tbody tr").count.should == Movie.all.count
+  end
+end
+
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
@@ -36,6 +67,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
     else
       check("ratings_#{value}")
     end
-       #"When I #{uncheck}" "ratings_#{value}"
+  end
+end
+
+Given /I really uncheck the following ratings: (.*)/ do |rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/, /).each do |value|
+    uncheck("ratings_#{value}")
   end
 end
